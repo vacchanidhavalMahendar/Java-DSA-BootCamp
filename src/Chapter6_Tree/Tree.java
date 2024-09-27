df1d9959280736cefe617a92b27136f787f93dbf
vacchanidhavalMahendar <dhavalvacchani1111@gmail.com>
2024-09-27 20:12:29 +0530

    my dsa commit2

---
+++ src/Chapter6_Tree/Tree.java
@@ -0,0 +1,18 @@
+package Chapter6_Tree;
+
+
+
+public class Tree {
+    int data;
+    Tree left;
+    Tree right;
+
+    Tree(int data)
+    {
+        this.data=data;
+        this.left=null;
+        this.right=null;
+    }
+}
+
+
